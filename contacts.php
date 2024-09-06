12e29a1f7a9c0b97937a4d316f03d66dfb6efe17
Diasso Marion <Diassomarion@gmail.com>
2024-09-06 10:11:53 +0000

    Ajout de la page Contacts.php et styles associer

---
+++ contacts.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>Contacts</title>
+    <link rel="stylesheet" href="style/header.css">
+    <link rel="stylesheet" href="style/footer.css">
+    <link rel="stylesheet" href="style/contacts.css">
+</head>
+
+<body>
+   
+    <?php require_once 'header.php'; ?>
+
+    
+    <div id="div_central">
+        <h1>Contactez-nous</h1>
+        <p>Suivez-nous sur les réseaux sociaux :</p>
+        <div class="social-icons">
+            <a href="https://www.facebook.com/share/xzv1HjrxeEDnbGmt/?mibextid=LQQJ4d" target="_blank">
+                <i class="fab fa-facebook fa-2x"></i>
+            </a>
+            <a href="tel:+22664584277" target="_blank">
+                <i class="fa fa-phone" afa-2></i>
+            </a>
+            <a href="mailto:01BP1091@bobo-dioulasso.com" target="_blank">
+                <i class="fas fa-envelope fa-2x"></i>
+            </a>
+        </div>
+        <p>Adresse: Bobo-Dioulasso, Burkina Faso</p>
+        <p>01 BP 1091 Bobo-Dioulasso 01, Burkina Faso</p>
+    </div>
+
+    <!-- Footer -->
+    <?php require_once 'footer.php'; ?>
+</body>
+
+</html>
